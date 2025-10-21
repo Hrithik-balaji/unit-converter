fc7983967f1d6253fe79ec02cffbbbfd568003a8
Hrithik-balaji <hrithikbalaji115@gmail.com>
2025-10-21 13:18:16 +0530

    Added km↔miles conversion functions

--- converter.py
+++ converter.py
@@ -6,3 +6,12 @@ def pounds_to_kg(lb):
 
 print("10 kg =", kg_to_pounds(10), "lb")
 print("22 lb =", pounds_to_kg(22), "kg")
+
+def km_to_miles(km):
+    return round(km * 0.621371, 2)
+
+def miles_to_km(miles):
+    return round(miles / 0.621371, 2)
+
+print("5 km =", km_to_miles(5), "miles")
+print("3 miles =", miles_to_km(3), "km")
